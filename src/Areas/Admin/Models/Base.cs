d0264f0496ddfd8777a1ea05cadd36b2829c6235
unknown <jasvir.nar@gmail.com>
2018-11-18 03:03:40 -0500

    added ui files

---
+++ src/Areas/Admin/Models/Base.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace JewelryBiz.UI.Areas.Admin.Models
+{
+    public class Base
+    {
+        protected JewelryBizOnlineEntities _ctx = new JewelryBizOnlineEntities();
+    }
+}
